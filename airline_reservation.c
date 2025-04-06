2784c1ce56dd24a8c4f87fcd23175932be833fd5
adityasajwan18 <adityasajwan18@gmail.com>
2025-04-06 23:22:31 +0530

    basic edits

--- airline_reservation.c
+++ airline_reservation.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
 #define FILENAME "reservations.txt"
 #define MAX_SEATS 50
-
 struct Passenger {
     char pnr[10];
     char name[50];
@@ -15,16 +13,12 @@ struct Passenger {
     char destination[50];
     int seatNo;
 };
-
-// Generate Random PNR
 char* generatePNR() {
     static char pnr[10];
     srand(time(0) + rand());
     sprintf(pnr, "PNR%04d", rand() % 10000);
     return pnr;
 }
-
-// Check if seat is already booked
 int isSeatBooked(int seatNo) {
     FILE *fp = fopen(FILENAME, "r");
     struct Passenger p;
@@ -37,11 +31,9 @@ int isSeatBooked(int seatNo) {
     fclose(fp);
     return 0;
 }
-
 void bookTicket() {
     FILE *fp = fopen(FILENAME, "a");
     struct Passenger p;
-
     printf("Enter Name: ");
     scanf(" %[^\n]", p.name);
     printf("Enter Age: ");
@@ -52,7 +44,6 @@ void bookTicket() {
     scanf(" %[^\n]", p.source);
     printf("Enter Destination: ");
     scanf(" %[^\n]", p.destination);
-
     do {
         printf("Enter Seat No (1-%d): ", MAX_SEATS);
         scanf("%d", &p.seatNo);
@@ -60,24 +51,19 @@ void bookTicket() {
             printf("Seat Already Booked! Choose another.\n");
         }
     } while (isSeatBooked(p.seatNo));
-
     strcpy(p.pnr, generatePNR());
-
     fprintf(fp, "%s|%s|%d|%c|%s|%s|%d\n", p.pnr, p.name, p.age, p.gender, p.source, p.destination, p.seatNo);
     fclose(fp);
 
     printf("Ticket Booked Successfully!\nYour PNR: %s\n", p.pnr);
 }
-
 void displayTicket() {
     FILE *fp = fopen(FILENAME, "r");
     struct Passenger p;
     char pnr[10];
     int found = 0;
-
     printf("Enter PNR: ");
     scanf("%s", pnr);
-
     while (fscanf(fp, "%[^|]|%[^|]|%d|%c|%[^|]|%[^|]|%d\n", p.pnr, p.name, &p.age, &p.gender, p.source, p.destination, &p.seatNo) != EOF) {
         if (strcmp(p.pnr, pnr) == 0) {
             printf("\nPNR: %s\nName: %s\nAge: %d\nGender: %c\nSource: %s\nDestination: %s\nSeat No: %d\n", p.pnr, p.name, p.age, p.gender, p.source, p.destination, p.seatNo);
@@ -85,21 +71,17 @@ void displayTicket() {
             break;
         }
     }
-
     if (!found) printf("Ticket not found!\n");
     fclose(fp);
 }
-
 void cancelTicket() {
     FILE *fp = fopen(FILENAME, "r");
     FILE *temp = fopen("temp.txt", "w");
     struct Passenger p;
     char pnr[10];
     int found = 0;
-
     printf("Enter PNR to Cancel: ");
     scanf("%s", pnr);
-
     while (fscanf(fp, "%[^|]|%[^|]|%d|%c|%[^|]|%[^|]|%d\n", p.pnr, p.name, &p.age, &p.gender, p.source, p.destination, &p.seatNo) != EOF) {
         if (strcmp(p.pnr, pnr) == 0) {
             found = 1;
@@ -109,23 +91,19 @@ void cancelTicket() {
     }
     fclose(fp);
     fclose(temp);
-
     remove(FILENAME);
     rename("temp.txt", FILENAME);
-
     if (found) {
         printf("Ticket Cancelled Successfully!\n");
     } else {
         printf("Ticket Not Found!\n");
     }
 }
-
 void modifyTicket() {
     cancelTicket();
     printf("Re-book your ticket with updated details:\n");
     bookTicket();
 }
-
 void showAllBookings() {
     FILE *fp = fopen(FILENAME, "r");
     struct Passenger p;
@@ -136,20 +114,16 @@ void showAllBookings() {
     }
     fclose(fp);
 }
-
 void checkSeatAvailability() {
     int booked = 0;
     FILE *fp = fopen(FILENAME, "r");
     struct Passenger p;
-
     while (fscanf(fp, "%[^|]|%[^|]|%d|%c|%[^|]|%[^|]|%d\n", p.pnr, p.name, &p.age, &p.gender, p.source, p.destination, &p.seatNo) != EOF) {
         booked++;
     }
     fclose(fp);
-
     printf("Seats Booked: %d\nSeats Available: %d\n", booked, MAX_SEATS - booked);
 }
-
 void menu() {
     int choice;
     do {
@@ -175,7 +149,6 @@ void menu() {
         }
     } while (1);
 }
-
 int main() {
     menu();
     return 0;
